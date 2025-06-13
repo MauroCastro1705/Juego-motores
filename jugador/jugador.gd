4ac9c5b8bcc8d139de116c88360edad0c66ec85e
Mauro Castro <maurocastruli@gmail.com>
2025-06-13 15:44:03 -0300

    ships

---
+++ jugador/jugador.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var max_speed: float = 300.0     # Velocidad máxima
+@export var aceleracion: float = 900.0   # Qué tan rápido acelera
+@export var frenado: float = 800.0      # Qué tan rápido se frena
+@onready var marker = $Marker2D #para instanciar balas
+@onready var marker2 = $Marker2D2
+@onready var timer_escudo = $Timer
+
+#bala
+@export var bala:PackedScene
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+
+	input_vector.x = Input.get_action_strength("derecha") - Input.get_action_strength("izquierda")
+	input_vector.y = Input.get_action_strength("abajo") - Input.get_action_strength("arriba")
+	if input_vector != Vector2.ZERO:
+		# move_toward = interpola
+		velocity = velocity.move_toward(input_vector * max_speed, aceleracion * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, frenado * delta)
+	move_and_slide()
+	if Input.is_action_just_pressed("disparar"):
+		_disparar()
+
+func _disparar():
+	pass
