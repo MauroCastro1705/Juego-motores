09f07e07ece536178177afabd76369172ea0554a
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2025-06-17 19:23:24 -0300

    comit

--- jugador/jugador.gd
+++ jugador/jugador.gd
@@ -12,17 +12,19 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-
 	input_vector.x = Input.get_action_strength("derecha") - Input.get_action_strength("izquierda")
 	input_vector.y = Input.get_action_strength("abajo") - Input.get_action_strength("arriba")
+	_handle_movement(input_vector, delta)
+	move_and_slide()	
+	_disparar()
+
+func _handle_movement(input_vector, delta):
 	if input_vector != Vector2.ZERO:
 		# move_toward = interpola
 		velocity = velocity.move_toward(input_vector * max_speed, aceleracion * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, frenado * delta)
-	move_and_slide()
-	if Input.is_action_just_pressed("disparar"):
-		_disparar()
 
 func _disparar():
-	pass
+	if Input.is_action_just_pressed("disparar"):
+		pass
